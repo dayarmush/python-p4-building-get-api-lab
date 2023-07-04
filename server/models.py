5a064c36ec18678274c764e93e75b0ae6f0ee697
Dovid Yarmush <yarmushda@gmail.com>
2023-07-04 09:58:42 -0400

    Complete db schemas

--- server/models.py
+++ server/models.py
@@ -12,9 +12,22 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+
+    serialize_rules = ('-baked_goods.bakery',)
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    serialize_rules = ('-bakery.baked_goods')
     
